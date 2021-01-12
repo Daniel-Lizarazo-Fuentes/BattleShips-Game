c26de68de16d77a09e64cb2b3ba683499c87f529
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2021-01-12 16:01:12 +0100

    stub of abstract ship class

---
+++ ships/src/Ship.java
@@ -0,0 +1,59 @@
+//abstract parent class ship
+public abstract class Ship {
+    private int size;
+    private int hitPoints;
+    private String name;
+
+    public Ship(int size, int hitPoints, String name) {
+        this.size = size;
+        this.hitPoints = hitPoints;
+        this.name = name;
+    }
+
+    /**
+     *
+     * @param size
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getSize() {
+        return this.size;
+    }
+    /**
+     *
+     * @param hitPoints
+     */
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getHitPoints() {
+        return this.hitPoints;
+    }
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getName() {
+        return this.name;
+    }
+
+}
