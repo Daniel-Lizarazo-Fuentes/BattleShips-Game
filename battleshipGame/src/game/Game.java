628c4039067c4096a45d6b4edd2558119239d8ef
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2021-01-20 23:17:05 +0100

    intermediary for manual placement

--- battleshipGame/src/game/Game.java
+++ battleshipGame/src/game/Game.java
@@ -5,6 +5,7 @@ import game.board.*;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
 
 
 public class Game {
@@ -277,14 +278,166 @@ public class Game {
     }
 
     public void fillBoardManual(Board board) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> positions = new ArrayList<>();
+
+        ArrayList<Carrier> carriers = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            carriers.add(new Carrier("carrier" + i, positions));
+        }
+        ArrayList<Battleship> battleships = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            battleships.add(new Battleship("battleship" + i, positions));
+        }
+        ArrayList<Destroyer> destroyers = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            destroyers.add(new Destroyer("destroyer" + i, positions));
+        }
+        ArrayList<SuperPatrol> superPatrols = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            superPatrols.add(new SuperPatrol("SuperPatrol" + i, positions));
+        }
+        ArrayList<PatrolBoat> patrolBoats = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            patrolBoats.add(new PatrolBoat("PatrolBoat" + i, positions));
+        }
+
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        for (Ship sh : carriers) {
+            System.out.println("Enter valid field");
+            String randomfield = sc.nextLine();
+            boolean validField = true;
+
+            for (int k = 0; k < sh.getSize(); k++) {
+                char randomColumn = randomfield.charAt(0);
+                int randomRow = Character.digit(randomfield.charAt(1), 10);
+                String randomInput = alphabet[randomColumn + k] + Integer.toString(randomRow);
+                if (randomColumn + k < 15) {
+                    //if index doesn't exist then it will just be set to false
+                    try {
+                        if (board.getFields().get(board.getFieldIndex(randomCoordinate)).getState() != boardPosition.positionState.EMPTY) {
+                            foundFittingField = false;
+                        }
+                    } catch (IndexOutOfBoundsException e) {
+                        foundFittingField = false;
+                    }
+
+                } else {
+                    foundFittingField = false;
+                }
+            }
+            while (board.getFieldIndex(randomfield) == -1 && !validField) {
+
+
+                System.out.println("Enter valid field");
+                randomfield = sc.nextLine();
+            }
+
+            for (int j = 0; j < sh.getSize(); j++) {
+
+
+                board.getFields().get(board.getFieldIndex(randomfield)).setState(boardPosition.positionState.SHIP);
+                board.getFields().get(board.getFieldIndex(randomfield)).setShipType("CV"); // CV is based on hull type, meaning generic aircarft carrier
+
+
+            }
+            System.out.println(board.toString());
+        }
+        for (Ship sh : battleships) {
+            System.out.println("Enter valid field");
+            String randomfield = sc.nextLine();
+
+            while (board.getFieldIndex(randomfield) == -1) {
+
+
+                System.out.println("Enter valid field");
+                randomfield = sc.nextLine();
+            }
+
+            for (int j = 0; j < sh.getSize(); j++) {
+
+
+                board.getFields().get(board.getFieldIndex(randomfield)).setState(boardPosition.positionState.SHIP);
+                board.getFields().get(board.getFieldIndex(randomfield)).setShipType("BB"); // BB is based on hull type, meaning heavy gun-armed vessel
+
+
+            }
+            System.out.println(board.toString());
+        }
+        for (Ship sh : destroyers) {
+            System.out.println("Enter valid field");
+            String randomfield = sc.nextLine();
+
+            while (board.getFieldIndex(randomfield) == -1) {
+
+
+                System.out.println("Enter valid field");
+                randomfield = sc.nextLine();
+            }
+
+            for (int j = 0; j < sh.getSize(); j++) {
+
+
+                board.getFields().get(board.getFieldIndex(randomfield)).setState(boardPosition.positionState.SHIP);
+                board.getFields().get(board.getFieldIndex(randomfield)).setShipType("DD"); // DD is based on hull type, meaning destroyer
+
+
+            }
+            System.out.println(board.toString());
+        }
+        for (Ship sh : superPatrols) {
+            System.out.println("Enter valid field");
+            String randomfield = sc.nextLine();
+
+            while (board.getFieldIndex(randomfield) == -1) {
+
+
+                System.out.println("Enter valid field");
+                randomfield = sc.nextLine();
+            }
+
+            for (int j = 0; j < sh.getSize(); j++) {
+
+
+                board.getFields().get(board.getFieldIndex(randomfield)).setState(boardPosition.positionState.SHIP);
+                board.getFields().get(board.getFieldIndex(randomfield)).setShipType("SV"); // SV is based on hull type (made up definition), meaning super patrol vessel
+
+
+            }
+            System.out.println(board.toString());
+        }
+        for (Ship sh : patrolBoats) {
+            System.out.println("Enter valid field");
+            String randomfield = sc.nextLine();
+
+            while (board.getFieldIndex(randomfield) == -1) {
+
+
+                System.out.println("Enter valid field");
+                randomfield = sc.nextLine();
+            }
+
+            for (int j = 0; j < sh.getSize(); j++) {
+
+
+                board.getFields().get(board.getFieldIndex(randomfield)).setState(boardPosition.positionState.SHIP);
+                board.getFields().get(board.getFieldIndex(randomfield)).setShipType("PV"); // PV is based on hull type, meaning patrol vessel
+
+
+            }
+            System.out.println(board.toString());
+        }
 
     }
+
 
     public static void main(String[] args) {
         Board board = new Board(true);
         board.toString();
-        Game game = new Game(true);
+        Game game = new Game(false);
         System.out.println(game.getPlayerBoard().toString());
+        System.out.println(game.getComputerBoard().toString());
     }
 }
 
