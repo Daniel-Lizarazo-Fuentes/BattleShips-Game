bb5f27b6d99918e82fc228d6d7010ad72a952a70
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2021-01-31 17:22:05 +0100

    fire not possible

---
+++ battleshipGame/src/game/exceptions/FireNotPossible.java
@@ -0,0 +1,10 @@
+package game.exceptions;
+
+public class FireNotPossible extends Exception {
+    private static final long serialVersionUID = 565642L;
+
+    public FireNotPossible(String msg) {
+        super(msg);
+    }
+
+}
